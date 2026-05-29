9c36ab21da5af2f959023169d125e9aa08ee2643
JAP-18 <akhujapbca@gmail.com>
2026-05-29 20:48:35 +0530

    feat: initialize frontend architecture with react query and mui

---
+++ frontend/src/main.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import { QueryClient } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
+
+import { BrowserRouter } from "react-router-dom";
+
+import { theme } from "./theme";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
