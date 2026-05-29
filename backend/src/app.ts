98be3f0bbaf0e163acc57df6ec94d1e8366458af
JAP-18 <akhujapbca@gmail.com>
2026-05-29 20:06:40 +0530

    feat: implement salary insights aggregation apis, ro=outes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import employeeRoutes from "./routes/employee.routes.js";
+import insightRoutes from "./routes/insight.routes.js";
 import { errorMiddleware } from "./middlewares/error.middleware.js";
 
 const app = express();
@@ -15,5 +16,7 @@ app.get("/health", (_, res) => {
 app.use("/employees", employeeRoutes);
 
 app.use(errorMiddleware);
+
+app.use("/insights", insightRoutes);
 
 export default app;
